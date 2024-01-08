040d181cf2887a6beae88453023981b4122f1ecd
gary900130 <gary900130@gmail.com>
2024-01-08 02:26:19 +0000

    Initial commit

--- LineBotApi.py
+++ LineBotApi.py
@@ -1,16 +1,28 @@
 from linebot import LineBotApi, WebhookHandler, exceptions
 from linebot.models import TextSendMessage, events
-from dotenv import load_dotenv
+from datetime import datetime
+import netifaces
 import os
+import logging
 
-load_dotenv()
+# 讀取 LINE BOT API
+CHANNEL_ACCESS_TOKEN = os.environ.get("lineBotToken")
 
-CHANNEL_ACCESS_TOKEN = os.getenv("lineBotToken")
+# 設定日誌格式
+logging.basicConfig(filename='example.log', level=logging.INFO,format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%s')
+logging.info('漢堡 啟動！')
 
-try:
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-    line_bot_api.broadcast(TextSendMessage(text="新年快樂"))
-except exceptions.LineBotApiError as e:
-    print(f"LineBotApiError: {e.status_code} {e.error.message}")
-except Exception as e:
-    print(f"Exception: {e}")
+# 取得IP位置
+ip = netifaces.ifaddresses('enp0s8')[netifaces.AF_INET][0]['addr']
+logging.info(f'找到你家地址了，在{ip}')
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+# 從日誌檔案中讀取最後兩行
+with open('example.log', 'r') as log:
+    last_two_lines = log.readlines()[-2:]
+
+# 對每一行進行廣播
+for line in last_two_lines:
+    line_bot_api.broadcast(TextSendMessage(text=line.strip()))
+
+# line_bot_api.broadcast(TextSendMessage(text="我是漢堡"))
